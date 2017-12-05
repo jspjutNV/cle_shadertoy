92c2c2f4acd6d3602f87527ab12651e14acf56f9
Josef Spjut <jspjut@nvidia.com>
2017-12-05 15:27:00 -0500

    First working implementation

--- tiled_display.glsl
+++ tiled_display.glsl
@@ -1,35 +1,16 @@
-# CLE Shadertoy
+#version 460
 
-This project uses [GeeXLab](http://www.geeks3d.com/geexlab/downloads/). 
-GeeXLab is available for Windows, MacOS, Linux, and Raspberry Pi.
-Please download it and make the following changes to run this project.
+uniform vec3      iResolution;     // viewport resolution (in pixels)
+uniform float     iTime;     // shader playback time (in seconds)
+uniform vec4      iMouse;          // mouse pixel coords. xy: current (if MLB down), zw: click
+uniform sampler2D iChannel0;
 
-First you need to update the path to the `GeeXLab.exe` executable in `start_shadertoy.bat`.
-For example, it could be the following:
+in vec2 fragCoord;
+out vec4 fragColor;
 
-```
-start C:\GeeXLab_FREE_win64\GeeXLab.exe /log_framerate /no_menubar /glinfo /demofile="tiled_display.xml"
-```
-
-
-## Notes
-
-* Image loading uses [gh_texture.create_from_file()](http://www.geeks3d.com/geexlab/reference/scripting_texture.php#create_from_file) or its relatives (v2, etc.)
-* * Call `create_from_file` inside `init_scene` 
-* * Call `gh_texture.bind(tex, 0)` inside `update_scene` with `gh_gpu_program.uniform1i(gpu_prog_01, "iChannel0", 0)` to bind it
-* Video loading/updating uses [gh_av.video_init_texture()](http://www.geeks3d.com/geexlab/reference/scripting_av.php#video_init_texture)
-* * Call `video_init_texture` inside `init_scene`
-* * As above, bind the texture inside `update_scene` but also call `gh_av.video_update_texture(av, tex_id)` to get a new frame
-
-I had to change `fragCoord` to `gl_FragCoord` to get the code to work in GeeXLab.
-
-Converting the shadertoy from https://www.shadertoy.com/view/Md2fWd into Falcor.
-
-Code below:
-```
-void mainImage( out vec4 fragColor, in vec2 fragCoord )
+void main()
 {
-	vec2 uv = fragCoord.xy / iResolution.xy;
+	vec2 uv = gl_FragCoord.xy / iResolution.xy;
     
 	fragColor = vec4(uv,0.5+0.5*sin(iTime),1.0);
 	
@@ -70,6 +51,7 @@ void mainImage( out vec4 fragColor, in vec2 fragCoord )
 //    fragColor = vec4(g);
 //    fragColor = vec4(b);
 
-}
+    // test direct texture map
+    // fragColor = vec4(uv, 0.0, 1.0);
 
-```
+}
